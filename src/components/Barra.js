47f58ca49e0e141ad10542b93a97f812a733234d
Francoedson <franco.edson.18014@gmail.com>
2023-09-05 13:14:28 -0500

    section navbar

--- src/components/Barra.js
+++ src/components/Barra.js
@@ -2,7 +2,7 @@ import { useState,useEffect} from "react";
 import { Navbar, Container,Nav} from "react-bootstrap";
 import logo from '../assets/img/logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon2 from '../assets/img/iconmonstr-github-1.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
 
 export const Barra = () => {
@@ -38,17 +38,17 @@ export const Barra = () => {
                 </Navbar.Toggle>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home" className={activeLink==='home' ? 'active navbar-link' : 'navbar-link'} onClick={()=>onUpdateActiveLink('home')}>Home</Nav.Link>
+                        <Nav.Link href="#home" className={activeLink==='home' ? 'active navbar-link' : 'navbar-link'} onClick={()=>onUpdateActiveLink('home')}>Sobre mí</Nav.Link>
                         <Nav.Link href="#projects" className={activeLink==='projects' ? 'active navbar-link' : 'navbar-link'} onClick={()=>onUpdateActiveLink('projects')}>Proyectos</Nav.Link>
                         <Nav.Link href="#skills" className={activeLink==='skills' ? 'active navbar-link' : 'navbar-link'}onClick={()=>onUpdateActiveLink('skills')}> Skills</Nav.Link>
                     </Nav>
                     <span className="navbar-text">
                         <div className="social-icon">
-                            <a href="#"><img src={navIcon1} alt=""/></a>
-                            <a href="#"><img src={navIcon2} alt=""/></a>
-                            <a href="#"><img src={navIcon3} alt=""/></a>
+                            <a target="_blank" href="https://www.linkedin.com/in/franco-mari%C3%B1o-2a289620a/"><img src={navIcon1} alt=""/></a>
+                            <a target="_blank" href="https://github.com/francoedson"><img src={navIcon2} alt=""/></a>
+                            <a target="_blank" href="https://www.instagram.com/franco.edson_/"><img src={navIcon3} alt=""/></a>
                         </div>
-                        <button className="vvd" onClick={()=>console.log('connect')}><span>Let's go</span></button>
+                        <button className="vvd" onClick={()=>console.log('connect')}><span>Contáctame</span></button>
                     </span>
                 </Navbar.Collapse>
             </Container>
