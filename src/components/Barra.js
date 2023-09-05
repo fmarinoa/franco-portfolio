c5da9b81301a0e2df33dea15af5950d9fe44d89a
Francoedson <franco.edson.18014@gmail.com>
2023-09-05 15:04:16 -0500

    section navbar

--- src/components/Barra.js
+++ src/components/Barra.js
@@ -1,6 +1,6 @@
 import { useState,useEffect} from "react";
 import { Navbar, Container,Nav} from "react-bootstrap";
-import logo from '../assets/img/logo.svg';
+import logo from '../assets/img/header-img.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/iconmonstr-github-1.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
