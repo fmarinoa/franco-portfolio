34becce2fd099cda28e305e4a6aa4560437bddf6
Francoedson <franco.edson.18014@gmail.com>
2023-09-05 17:45:47 -0500

    modify projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,62 +1,74 @@
-import { Col, Container, Row, Tab } from "react-bootstrap"
+import { Col, Container, Row, Tab, Carousel } from "react-bootstrap";
 import projImg1 from '../assets/img/project-img1.png';
 import projImg2 from '../assets/img/project-img2.png';
 import projImg3 from '../assets/img/project-img3.png';
 import colorSharp2 from '../assets/img/color-sharp2.png';
-import { ProjectsCards } from "./ProjectsCards";
-
 
 export const Projects = () => {
     const projects = [
-        {
-            title: "Sistema de Gestión de Contactos Web",
-            description: "Crea, edita, y gestiona contactos fácilmente, recibe correos de bienvenida y exporta a Excel con este completo proyecto web.",
-            imgUrl: projImg1,
-            link: "http://www.miscontactos.somee.com"
-        },
-        {
-            title: "Licorería demo: MiMundo",
-            description: "Explora nuestro catálogo digital de licores de alta calidad y encuentra tus favoritos en un solo lugar.",
-            imgUrl: projImg2,
-            link: "https://licoreriamimundo.netlify.app/"
-        },
-        {
-            title: "TaskMaker",
-            description: "Agrega tareas en una interfaz muy intuitiva que se guardan en una base de datos en línea. También te permite marcar estas tareas como completadas.",
-            imgUrl: projImg3,
-            link: "http://www.taskmaker.somee.com"
-        },
-    ]
-
+      {
+        title: "Sistema de Gestión de Contactos Web",
+        description:
+          "Crea, edita, y gestiona contactos fácilmente, recibe correos de bienvenida y exporta a Excel con este completo proyecto web.",
+        imgUrl: projImg1,
+        link: "http://www.miscontactos.somee.com",
+        repo:"https://github.com/francoedson/ContactoCRUD"
+      },
+      {
+        title: "Licorería demo: MiMundo",
+        description:
+          "Explora nuestro catálogo digital de licores de alta calidad y encuentra tus favoritos en un solo lugar.",
+        imgUrl: projImg2,
+        link: "https://licoreriamimundo.netlify.app/",
+        repo:"https://github.com/francoedson/LicoreriaMiMundo"
+      },
+      {
+        title: "TaskMaker",
+        description:
+          "Agrega tareas en una interfaz muy intuitiva que se guardan en una base de datos en línea. También te permite marcar estas tareas como completadas.",
+        imgUrl: projImg3,
+        link: "http://www.taskmaker.somee.com",
+        repo:"https://github.com/francoedson/ReactTasker"
+      },
+    ];
+  
     return (
-        <section className="project" id="projects">
-            <Container>
-                <Row>
-                    <Col>
-                        <h2>Proyectos</h2>
-                        <p>Te invito a dale un vistazo a algunos de los proyectos que he realizado</p>
-                        <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                        <Tab.Content>
-                            <Tab.Pane eventKey="first">
-                                <Row>
-                                    {
-                                        projects.map((projects,index)=>{
-                                            return(
-                                                <ProjectsCards
-                                                    key={index}
-                                                    {...projects}
-                                                    />
-                                            )
-                                        })
-                                    }
-                                </Row>
-                            </Tab.Pane>
-                        </Tab.Content>
-                        </Tab.Container>
-                    </Col>
-                </Row>
-            </Container>
-            <img className="background-image-right" src={colorSharp2}/>
-        </section>
-    )
-}
+      <section className="project" id="projects">
+        <Container>
+          <Row>
+            <Col>
+            <div className="skill-bx">
+              <h2>Proyectos</h2>
+              <p>Te invito a darle un vistazo a algunos de los proyectos que he realizado</p>
+              <Carousel interval={3000}>
+                {projects.map((project, index) => {
+                  return (
+                    <Carousel.Item key={index}>
+                      <Row>
+                        <Col sm={6}>
+                          <img src={project.imgUrl} alt={project.title} className="img-fluid" />
+                        </Col>
+                        <Col sm={6}>
+                          <h3>{project.title}</h3>
+                          <p>{project.description}</p>
+                          <a href={project.link} target="_blank" rel="noopener noreferrer">
+                            Ver proyecto
+                          </a><br></br>
+                          <a href={project.repo} target="_blank" rel="noopener noreferrer">
+                            Ver repositorio
+                          </a>
+                        </Col>
+                      </Row>
+                    </Carousel.Item>
+                  );
+                })}
+              </Carousel>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+        <img className="background-image-right" src={colorSharp2} alt="Background" />
+      </section>
+    );
+  };
+  
